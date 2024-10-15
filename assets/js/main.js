0b16f94903cdc1f2e0f4c2f75018b4d0ac5437ed
restumi <maulanaismael5@gmail.com>
2024-10-15 11:39:45 +0700

    noMassage

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('navMenu'),
+      navToggle = document.getElementById('navToggle'),
+      navClose = document.getElementById('navClose')
+
+/* Menu show */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/* Menu hidden */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.navLink')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('navMenu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== ADD BLUR TO HEADER ===============*/
+
+
+/*=============== EMAIL JS ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
