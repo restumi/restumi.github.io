286eb6ada291b4b1436653bc5df7af1e871598db
restumi <maulanaismael5@gmail.com>
2024-10-27 21:09:10 +0700

    add scrollBar, scrollUp, and active link in navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,17 +47,61 @@ const sendEmail = (e) => {
     emailjs.sendForm('service_hj8n2w9', 'template_kn9dci6', '#contactForm', 'LAUwd0vWdorzQS6-V' )
         .then(() => {
             contactMessage.textContent = 'Pesan Terkirim✅'
+
+            setTimeout(() => {
+                contactMessage.textContent = ''
+            }, 2500)
+
+            contactForm.reset()
         },() => {
             contactMessage.textContent = 'Pesan gagal terkirim (ada kesalahan) ❌'
+
+            setTimeout(() => {
+                contactMessage.textContent = ''
+            }, 2500)
+
+            contactForm.reset()
         })
 }
 
 contactForm.addEventListener('submit', sendEmail)
 
 /*=============== SHOW SCROLL UP ===============*/ 
+// const scrollUp = () => {
+//     const scrollUp = document.getElementsById('scroll-up')
+
+//     this.scrollY >= 350 ? scrollUp.classList.add('showScroll')
+//                     : scrollUp.classList.remove('showScroll')
+// }
+
+// window.addEventListener('scroll', scrollUp)
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scroll-up class
+    if(this.scrollY >= 500) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+  }
+  window.addEventListener('scroll', scrollUp)
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollY = window.pageYOffset
 
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.navMenu a[href*=' + sectionId + ']')
+
+		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
